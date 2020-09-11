73e0c3d215244810c894b3204d6403df5c343708
Kumar Sahab <krsahab@outlook.com>
2020-09-11 13:09:28 +0530

    Updating Links

--- RacingBattlegrounds.UI/Controllers/HomeController.cs
+++ RacingBattlegrounds.UI/Controllers/HomeController.cs
@@ -12,5 +12,13 @@ namespace RacingBattlegrounds.UI.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
