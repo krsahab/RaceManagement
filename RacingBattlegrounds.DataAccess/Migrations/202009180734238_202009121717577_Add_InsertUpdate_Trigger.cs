9af354598cde1b5ab863aaf4b60230009346cab3
Kumar Sahab <krsahab@outlook.com>
2020-09-18 13:09:42 +0530

    Updating Triggers

---
+++ RacingBattlegrounds.DataAccess/Migrations/202009180734238_202009121717577_Add_InsertUpdate_Trigger.cs
@@ -0,0 +1,43 @@
+﻿namespace RacingBattlegrounds.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class _202009121717577_Add_InsertUpdate_Trigger : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"CREATE OR ALTER TRIGGER tr_Participants_ForInsert
+ON Participants
+FOR INSERT
+AS
+BEGIN
+	UPDATE Participants
+	SET IsWinner = 
+	CASE
+	WHEN CompletionTime = (SELECT MIN(CompletionTime) FROM Participants WHERE Race_Id = (SELECT Race_Id FROM inserted)) THEN 1
+	ELSE 0
+	END
+	WHERE Race_Id = (SELECT Race_Id FROM inserted)
+END");
+            Sql(@"CREATE OR ALTER TRIGGER tr_Participants_ForUpdate
+ON Participants
+FOR UPDATE
+AS
+BEGIN
+	UPDATE Participants
+	SET IsWinner = 
+	CASE
+	WHEN CompletionTime = (SELECT MIN(CompletionTime) FROM Participants WHERE Race_Id = (SELECT Race_Id FROM inserted)) THEN 1
+	ELSE 0
+	END
+	WHERE Race_Id = (SELECT Race_Id FROM inserted)
+END");
+        }
+
+        public override void Down()
+        {
+            Sql("DROP TRIGGER tr_Participants_ForInsert");
+            Sql("DROP TRIGGER tr_Participants_ForUpdate");
+        }
+    }
+}
